48a9a11d8305c4af87b2f87e8a67408ef4e5a985
samarjit98 <karmakar.samarjit@gmail.com>
2019-09-30 23:29:49 +0530

    encrypting

--- DES/des_encryption.c
+++ DES/des_encryption.c
@@ -2,8 +2,95 @@
 #include <stdlib.h>
 #include <string.h>
 
+int S_box[8][4][16], Init_P[8][8], Final_P[8][8], ED[8][6], SD[4][8], CD[6][8];
+
+void binarize(int *data, int n, int val){
+	for(int i=n-1; i>=0; i--){
+		data[i] = val%2;
+		val /= 2;
+	}
+}
+
+void init_permutation(int data[64]){
+	int data_cpy[64];
+	for(int i=0; i<64; i++)data_cpy[i]=data[i];
+
+	for(int i=0; i<8; i++)
+		for(int j=0; j<8; j++)
+			data[8*i + j] = data_cpy[Init_P[i][j] - 1];
+}
+
+void final_permutation(int data[64]){
+	int data_cpy[64];
+	for(int i=0; i<64; i++)data_cpy[i]=data[i];
+
+	for(int i=0; i<8; i++)
+		for(int j=0; j<8; j++)
+			data[8*i + j] = data_cpy[Final_P[i][j] - 1];
+}
+
+void permutation_box(int per_data[6], int i, int per_out[4]){
+	int row = 2*per_data[0] + per_data[5];
+	int col = 8*per_data[1] + 4*per_data[2] + 2*per_data[3] + per_data[4];
+
+	binarize(per_out, 4, S_box[i][row][col]);
+}
+
+void round_function(int data[32], int key[48]){
+	int exp_data[48];
+
+	for(int i=0; i<8; i++)
+		for(int j=0; j<6; j++)
+			exp_data[6*i + j] = data[ED[i][j] - 1];
+
+	for(int i=0; i<48; i++)exp_data[i] = exp_data[i]^key[i];
+
+	for(int i=0; i<8; i++){
+		int per_data[6];
+		for(int j=0; j<6; j++)per_data[j]=exp_data[6*i + j];
+
+		int per_out[4];
+		permutation_box(per_data, i, per_out);
+
+		for(int j=0; j<4; j++)data[4*i + j] = per_out[j];
+	}
+
+	int data_cpy[32];
+	for(int i=0; i<32; i++)data_cpy[i]=data[i];
+
+	for(int i=0; i<4; i++)
+		for(int j=0; j<8; j++)
+			data[8*i + j] = data_cpy[SD[i][j] - 1];
+}
+
+void encrypt(int data[64], int key[8][48]){
+	int L[32], R[32];
+
+	for(int i=0; i<64; i++)
+		if(i<32)L[i] = data[i];
+		else R[i - 32] = data[i];
+
+	for(int i=0; i<16; i++){
+		int R_cpy[32];
+
+		for(int i=0; i<32; i++)R_cpy[i] = R[i];
+
+		round_function(R, key[i]);
+
+		for(int i=0; i<32; i++)L[i] = L[i]^R[i];
+
+		if(i<15){
+			for(int i=0; i<32; i++)R[i] = L[i];
+			for(int i=0; i<32; i++)L[i] = R_cpy[i];
+		}
+	}
+
+	for(int i=0; i<64; i++)
+		if(i<32)data[i] = L[i];
+		else data[i] = R[i - 32];
+}
+
 int main(int argc, char* argv[]){
-	int S_box[8][4][16], Init_P[8][8], Final_P[8][8], ED[8][6], SD[4][8];
 	/*
 	Init S and P boxes
 	*/
@@ -47,6 +134,13 @@ int main(int argc, char* argv[]){
 	for(int i=0; i<8; i++)
 		for(int j=0; j<8; j++){
 			fscanf(f, "%d", &Final_P[i][j])
+		}
+	fclose(f);
+
+	f = fopen("CD.txt", "r");
+	for(int i=0; i<6; i++)
+		for(int j=0; j<8; j++){
+			fscanf(f, "%d", &CD[i][j])
 		}
 	fclose(f);
 
